86e76f54736c8c5c97d8a967b302f6fa6e616f9d
Robbie_Malunga <46316955+iamrobbiejr@users.noreply.github.com>
2023-03-01 21:45:37 +0200

    - model updated

--- app/Models/AlbumSong.php
+++ app/Models/AlbumSong.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class AlbumSong extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['song_id', 'album_id'];
 }
